96dcca4080fd422b461ecabe86dd4b327d5ded28
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2022-01-15 18:03:48 -0500

    Switch to more helpful regexp test assertion (#317)

--- test/lib/dependency-versions-test.ts
+++ test/lib/dependency-versions-test.ts
@@ -414,10 +414,8 @@ describe('Utils | dependency-versions', function () {
       ]);
 
       // Existing newline at end of file should be maintained.
-      // package1 should not end in newline
-      expect(!packageJson1Contents.endsWith('\n')).toBeTruthy();
-      // package2 should end in newline
-      expect(packageJson2Contents.endsWith('\n')).toBeTruthy();
+      expect(packageJson1Contents).not.toMatch(/\n$/); // package1 should not end in newline
+      expect(packageJson2Contents).toMatch(/\n$/); // package2 should end in newline
     });
   });
 
