3485d22161ad5de277e97ac2d9fbd7d773666355
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2023-05-23 13:34:52 -0400

    feat!: consider optionalDependencies by default

--- lib/defaults.ts
+++ lib/defaults.ts
@@ -3,5 +3,7 @@ import { DEPENDENCY_TYPE } from './types.js';
 export const DEFAULT_DEP_TYPES = [
   DEPENDENCY_TYPE.dependencies,
   DEPENDENCY_TYPE.devDependencies,
+  DEPENDENCY_TYPE.optionalDependencies,
   DEPENDENCY_TYPE.resolutions,
+  // peerDependencies is not included by default, see discussion in: https://github.com/bmish/check-dependency-version-consistency/issues/402
 ];
